fb04d190d9e8024deee76c146f4b17c0a5d5d175
brandly <brandly13@gmail.com>
2014-05-03 15:40:33 -0400

    init

---
+++ src/bf.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "io/ioutil"
+)
+
+func brainfuck(src, input string) {
+    var tape = []int{0}
+    var tapeIndex = 0
+    fmt.Println(tape)
+
+    for i := range src {
+        var char = string(src[i])
+
+        switch {
+        case char == ">":
+            tapeIndex += 1
+            if len(tape) <= tapeIndex
+                fmt.Println('extending tape')
+                tape = tape.append(0)
+
+        case char == "<":
+            if tapeIndex > 0 {
+                tapeIndex -= 1
+            }
+            fmt.Println("<")
+
+        case char == "+":
+            tape[tapeIndex] += 1
+            fmt.Println("+")
+
+        case char == "-":
+            tape[tapeIndex] -= 1
+            fmt.Println("-")
+
+        case char == ".":
+            fmt.Println(".")
+
+        case char == ",":
+            fmt.Println(",")
+
+        case char == "[":
+            fmt.Println("[")
+
+        case char == "]":
+            fmt.Println("]")
+
+        }
+    }
+}
+
+func main() {
+    content, err := ioutil.ReadFile(os.Args[1])
+    if err != nil {
+        fmt.Println("Error reading file")
+        fmt.Println(err)
+    } else {
+        var src = string(content)
+        var input = os.Args[2]
+
+        brainfuck(src, input)
+    }
+}
